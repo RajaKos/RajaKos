7e4c0c0e606e353407b205a2510cc993d8f74058
SMK Telkom SP Malang <vinnykartika10@gmail.com>
2016-11-18 19:59:11 +0700

    vinny commit3

--- app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/MainActivity.java
+++ app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/MainActivity.java
@@ -1,18 +1,9 @@
 package xiirpl508182838.project.smktelkom_mlg.sch.id;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
-import static xiirpl508182838.project.smktelkom_mlg.sch.id.R.styleable.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout dr;
@@ -21,61 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tl = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(tl);
-        ProsesNavigasi();
-    }
-
-    private void ProsesNavigasi() {
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                int id = menuItem.getItemId();
-
-                switch (id) {
-                    case R.id.home:
-                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
-                        dr.closeDrawers();
-                        break;
-                    case R.id.settings:
-                        Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.help:
-                        Toast.makeText(getApplicationContext(), "Petunjuk", Toast.LENGTH_SHORT).show();
-                        dr.closeDrawers();
-                        break;
-                    case R.id.add:
-                        Toast.makeText(getApplicationContext(), "Tambah", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.logout:
-
-
-                }
-                return true;
-            }
-        });
-        View header = navigationView.getHeaderView(0);
-        TextView tv_email = (TextView) header.findViewById(R.id.tv_email);
-        tv_email.setText("Raja Kos");
-        dr = (DrawerLayout) findViewById(R.id.drawer);
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, dr, tl, R.string.drawer_open, R.string.drawer_close) {
-
-            @Override
-            public void onDrawerClosed(View v) {
-                super.onDrawerClosed(v);
-            }
-
-            @Override
-            public void onDrawerOpened(View v) {
-                super.onDrawerOpened(v);
-            }
-        };
-        dr.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        setContentView(R.layout.login);
     }
 }
+
 
