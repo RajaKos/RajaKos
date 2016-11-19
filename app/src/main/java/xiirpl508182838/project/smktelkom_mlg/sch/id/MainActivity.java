1bda6f8248d4ceb500c0d18a21b048568e8f9af8
aldi <fuad_rizaldy_23rpl@student.smktelkom-mlg.sch.id>
2016-11-19 13:28:20 +0700

    Login awal

--- app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/MainActivity.java
+++ app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/MainActivity.java
@@ -1,100 +1,77 @@
 package xiirpl508182838.project.smktelkom_mlg.sch.id;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private DrawerLayout dr;
-    private Toolbar tl;
-    private FirebaseAuth firebaseAuth;
-    private DatabaseReference databaseReference;
-
+    FirebaseAuth firebaseAuth;
+    private EditText etEmail;
+    private EditText etPassword;
+    private TextView tvRegist;
+    private Button btLogin;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        if (firebaseAuth.getCurrentUser() == null) {
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
-        }
+        progressDialog = new ProgressDialog(this);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
+        etPassword = (EditText) findViewById(R.id.editTextPassword);
+        tvRegist = (TextView) findViewById(R.id.linkSignup);
+        btLogin = (Button) findViewById(R.id.buttonLogin);
 
-
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-
-        tl = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(tl);
-        ProsesNavigasi();
+        btLogin.setOnClickListener(this);
+        tvRegist.setOnClickListener(this);
     }
 
-    private void ProsesNavigasi() {
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+    private void userLogin() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        progressDialog.setMessage("Login in...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                int id = menuItem.getItemId();
-
-                switch (id) {
-                    case R.id.home:
-                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
-                        dr.closeDrawers();
-                        break;
-                    case R.id.settings:
-                        Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.help:
-                        Toast.makeText(getApplicationContext(), "Petunjuk", Toast.LENGTH_SHORT).show();
-                        dr.closeDrawers();
-                        break;
-                    case R.id.logout:
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Login Succesfully!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed! Please Try Again", Toast.LENGTH_SHORT).show();
                 }
-                return true;
             }
         });
-        View header = navigationView.getHeaderView(0);
-        TextView tv_email = (TextView) header.findViewById(R.id.tv_email);
-        tv_email.setText("Raja Kos");
-        dr = (DrawerLayout) findViewById(R.id.drawer);
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, dr, tl, R.string.drawer_open, R.string.drawer_close) {
-
-            @Override
-            public void onDrawerClosed(View v) {
-                super.onDrawerClosed(v);
-            }
-
-            @Override
-            public void onDrawerOpened(View v) {
-                super.onDrawerOpened(v);
-            }
-        };
-        dr.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public void onClick(View v) {
 
+        if (v == btLogin) {
+            userLogin();
+        }
+        if (v == tvRegist) {
+            finish();
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
     }
 }
 
---
+++ app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/RegisterActivity.java
@@ -0,0 +1,18 @@
+package xiirpl508182838.project.smktelkom_mlg.sch.id;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aldi on 11/19/2016.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reg);
+    }
+}
