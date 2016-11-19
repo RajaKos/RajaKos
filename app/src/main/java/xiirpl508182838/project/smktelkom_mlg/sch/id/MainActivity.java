72148958b03d9a36c533ce38377550555ad7fb67
aldi <fuad_rizaldy_23rpl@student.smktelkom-mlg.sch.id>
2016-11-19 16:39:42 +0700

    Login2

--- app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/MainActivity.java
+++ app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,6 +32,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.login);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+        }
 
         progressDialog = new ProgressDialog(this);
 
@@ -47,16 +52,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String email = etEmail.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
 
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         progressDialog.setMessage("Login in...");
         progressDialog.show();
 
-        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
                 if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Login Succesfully!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Login Failed! Please Try Again", Toast.LENGTH_SHORT).show();
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
                 }
             }
         });
