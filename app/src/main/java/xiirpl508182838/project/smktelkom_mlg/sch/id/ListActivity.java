8a2fa0ca712d635bfd9823abb2ec7180d8374f46
ayu <ayusetyaningtyas16@gmail.com>
2016-11-20 14:24:52 +0700

    List commit2

--- app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/ListActivity.java
+++ app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/ListActivity.java
@@ -1,13 +1,26 @@
 package xiirpl508182838.project.smktelkom_mlg.sch.id;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
+import xiirpl508182838.project.smktelkom_mlg.sch.id.adapter.KostAdapter;
+import xiirpl508182838.project.smktelkom_mlg.sch.id.model.Kost;
+
 public class ListActivity extends AppCompatActivity {
+
+    ArrayList<Kost> mList = new ArrayList<>();
+    KostAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,32 @@ public class ListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new KostAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+    }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.places);
+        String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.place_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+
+        for (int i = 0; i < arFoto.length; i++) {
+            arFoto[i] = a.getDrawable(i);
+        }
+        a.recycle();
+
+        for (int i = 0; i < arJudul.length; i++) {
+            mList.add(new Kost(arJudul[i], arDeskripsi[i], arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
     }
 
 }
