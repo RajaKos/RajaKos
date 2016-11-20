27d81e23bc11129757100afa76d29353b3c859ca
SMK Telkom SP Malang <vinnykartika10@gmail.com>
2016-11-20 12:52:49 +0700

    manggil registrasi

---
+++ app/src/main/java/xiirpl508182838/project/smktelkom_mlg/sch/id/TambahActivity.java
@@ -0,0 +1,13 @@
+package xiirpl508182838.project.smktelkom_mlg.sch.id;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TambahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reg);
+    }
+}
